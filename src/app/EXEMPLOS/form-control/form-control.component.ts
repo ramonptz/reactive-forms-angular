ea7e74e61f252a9272c4d46fcebf6a347ee1b0e9
Ramon <ramonboladao98@gmail.com>
2024-11-04 23:40:39 -0300

    aula260

---
+++ src/app/EXEMPLOS/form-control/form-control.component.ts
@@ -0,0 +1,30 @@
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-control',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './form-control.component.html',
+  styleUrl: './form-control.component.scss'
+})
+export class FormControlComponent implements OnInit {
+  
+  ngOnInit() {
+    console.log(this.nome);
+  }
+  
+  nome = new FormControl('', [Validators.required]);
+  
+  status() {
+    console.log(this.nome);
+  }
+  
+  alterarValor() {
+    this.nome.setValue('Alterado !!!');
+  }
+  
+  inputAlterado(event: Event) {
+    console.log(event);
+  }
+}
